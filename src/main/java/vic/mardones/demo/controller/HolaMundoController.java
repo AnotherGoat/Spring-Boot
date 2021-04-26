577712b0e97b5fa79e9dc92ed36debc1f1ce2c20
Víctor Mardones <v.mardones04@ufromail.cl>
2021-04-26 07:54:35 -0400

    Commit inicial

---
+++ src/main/java/vic/mardones/demo/controller/HolaMundoController.java
@@ -0,0 +1,15 @@
+package vic.mardones.holamundo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(value = "/api")
+public class HolaMundoController {
+
+    @GetMapping(value = "/holamundo")
+    public String desplegarHolaMundo() {
+        return "Hola mundo";
+    }
+}
